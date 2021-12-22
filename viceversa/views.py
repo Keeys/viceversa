bb88c42404460873e345555a191fe8d566400477
Dmitrii <dmitrii@Mini-Dmitrii.N1>
2021-12-22 20:29:04 +0300

    Finish

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,8 +7,16 @@ def home(request):
 def reverse(request):
     # get from home html 'name=usertext'
     user_text = request.GET['usertext'] 
-    reversed_text = user_text[::-1] 
+
+    # split string and calculate number of words
+    word_list = user_text.split()
+    number_of_words = len(word_list)
+  
+    # reverse entered text in string
+    reversed_text = user_text[::-1]
+
     return render(request, 'reverse.html', {
         'usertext':user_text, 
-        'reversedtext':reversed_text
+        'reversedtext':reversed_text,
+        'numberofwords':number_of_words
         })
